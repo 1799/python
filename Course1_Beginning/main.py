8d14297ef602b5d413ff4cac0cb68805e9f2810b
Quang Phu <quangphu1799@gmail.com>
2019-01-13 16:59:32 +0700

    Exercise 1

--- Course1_Beginning/main.py
+++ Course1_Beginning/main.py
@@ -1,3 +1,11 @@
-print("hello")
-
-ds
+#calculate total pay base on worked hours
+def exercise_1():
+    hrs = input("Enter hours:")
+    h = float(hrs)
+    rate = input("Enter rate:")
+    r = float(rate)
+    if h > 40:
+        return (h - 40)*1.5*r + 40*r
+    else:
+        return h*r
+print(exercise_1())
